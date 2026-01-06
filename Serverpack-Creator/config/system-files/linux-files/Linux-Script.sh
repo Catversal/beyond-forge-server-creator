52b6e486d3fa1d8dcc4dc12587af5f335cee8eb6
Catversal <mmoga.my@gmail.com>
2026-01-06 17:13:22 +0100

    Enhance client pack validation in Linux-Script.sh
    Updated the script to check for the completeness of the client pack folder and provided user instructions if it is incomplete.

--- Serverpack-Creator/config/system-files/linux-files/Linux-Script.sh
+++ Serverpack-Creator/config/system-files/linux-files/Linux-Script.sh
@@ -2,6 +2,67 @@
 
 # Script to Update the Script installer
 
-echo test
+echo "Creating Client Pack Directory..."
+
+sleep 2.
+
+mkdir -p "$Main_Path/client_pack"
+
+# Check CLient Pack Directory
+
+while true; do
+
+clear
 echo
-read -p "Press Enter to start..."
+echo ============================================
+echo     Serverpack Builder - Beyond Packs
+echo ============================================
+echo by Catversal
+echo
+echo ====================================================
+echo  Checking Client Pack Folder...
+echo ====================================================
+echo
+sleep 2
+
+if [ -f "$CLIENT_PACK/config" ]; then
+    if [ -f "$CLIENT_PACK/mods" ]; then
+        if [ -f "$CLIENT_PACK/manifest.json" ]; then
+
+            echo "Client Pack Found! Complete Continuing..."
+            sleep 2
+            break
+        fi
+    fi
+else
+
+    echo ============================================
+    echo
+    echo     Serverpack Builder - Beyond Packs
+    echo ============================================
+    echo by Catversal
+    echo
+    echo
+    echo ====================================================
+    echo  Your client pack folder isnt complete.
+    echo ====================================================
+    echo
+    echo Please copy the CONTENT of the Beyond modpack into this folder:
+    echo
+    echo   $CLIENT_PACK
+    echo
+    echo Instructions:
+    echo
+    echo 1. Open CurseForge
+    echo 2. Select the downloaded modpack
+    echo 3. Click the three dots next to the Play button
+    echo 4. Click "Open Folder"
+    echo 5. Copy the CONTENT of this folder into the client_pack directory
+    echo
+    echo IMPORTANT:
+    echo - Copy ONLY the contents all files and folders INSIDE the modpack folder...
+    echo - Do NOT copy the parent folder itself
+    echo
+    read -p "Press ENTER once you have finished copying the files."
+
+if
