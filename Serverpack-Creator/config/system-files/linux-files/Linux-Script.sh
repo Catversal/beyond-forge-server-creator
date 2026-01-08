c9d245d68468068ff742dd849fbb8bf657852265
Catversal <mmoga.my@gmail.com>
2026-01-08 20:01:22 +0100

    Update Linux-Script.sh

--- Serverpack-Creator/config/system-files/linux-files/Linux-Script.sh
+++ Serverpack-Creator/config/system-files/linux-files/Linux-Script.sh
@@ -12,82 +12,82 @@ mkdir -p "$Main_Path/client_pack"
 
 while true; do
 
-clear
-echo
-echo ============================================
-echo     Serverpack Builder - Beyond Packs
-echo ============================================
-echo by Catversal
-echo
-echo ====================================================
-echo  Checking Client Pack Folder...
-echo ====================================================
-echo
-sleep 2
+  clear
+  echo
+  echo ============================================
+  echo     Serverpack Builder - Beyond Packs
+  echo ============================================
+  echo by Catversal
+  echo
+  echo ====================================================
+  echo  Checking Client Pack Folder...
+  echo ====================================================
+  echo
+  sleep 2
 
-if [ -d "$Client_Pack/config" ] \
-   && [ -d "$Client_Pack/mods" ] \
-   && [ -f "$Client_Pack/manifest.json" ]; then
+  if [ -d "$Client_Pack/config" ] \
+     && [ -d "$Client_Pack/mods" ] \
+     && [ -f "$Client_Pack/manifest.json" ]; then
 
-    echo "Client Pack Found! Complete Continuing..."
-    sleep 2
-    break
+      echo "Client Pack Found! Complete Continuing..."
+      sleep 2
+      break
 
-else
+  else
 
-    echo ============================================
-    echo     Serverpack Builder - Beyond Packs
-    echo ============================================
-    echo by Catversal
-    echo
-    echo
-    echo ====================================================
-    echo  Your client pack folder isnt complete.
-    echo ====================================================
-    echo
-    echo Please copy the CONTENT of the Beyond modpack into this folder:
-    echo
-    echo   $Client_Pack
-    echo
-    echo Instructions:
-    echo
-    echo 1. Open CurseForge
-    echo 2. Select the downloaded modpack
-    echo 3. Click the three dots next to the Play button
-    echo 4. Click "Open Folder"
-    echo 5. Copy the CONTENT of this folder into the client_pack directory
-    echo
-    echo IMPORTANT:
-    echo - Copy ONLY the contents all files and folders INSIDE the modpack folder...
-    echo - Do NOT copy the parent folder itself
-    echo
-    echo ====================================================
-    echo Info:
-    echo ====================================================
-    echo
+      echo ============================================
+      echo     Serverpack Builder - Beyond Packs
+      echo ============================================
+      echo by Catversal
+      echo
+      echo
+      echo ====================================================
+      echo  Your client pack folder isnt complete.
+      echo ====================================================
+      echo
+      echo Please copy the CONTENT of the Beyond modpack into this folder:
+      echo
+      echo   $Client_Pack
+      echo
+      echo Instructions:
+      echo
+      echo 1. Open CurseForge
+      echo 2. Select the downloaded modpack
+      echo 3. Click the three dots next to the Play button
+      echo 4. Click "Open Folder"
+      echo 5. Copy the CONTENT of this folder into the client_pack directory
+      echo
+      echo IMPORTANT:
+      echo - Copy ONLY the contents all files and folders INSIDE the modpack folder...
+      echo - Do NOT copy the parent folder itself
+      echo
+      echo ====================================================
+      echo Info:
+      echo ====================================================
+      echo
 
-    # 
-    # if [ -d "$Client_Pack/config" ] then
-    #     sleep 1 
-    # else
-    #     echo "- The 'config' folder is missing"
-    # fi
-    # if [ -d "$Client_Pack/mods" ] then
-    #   sleep 1
-    # else
-    #     echo "- The 'mods' folder is missing"
-    # fi
-    # 
-    # if [ -f "$Client_Pack/manifest.json" ]  then
-    #    sleep 1
-    # else
-    #     echo "- The 'manifest.json' file is missing"
-    # fi
-    # */
-    echo
-    read -p "Press ENTER once you have finished copying the files."
-fi
-
+      # 
+      # if [ -d "$Client_Pack/config" ] then
+      #     sleep 1 
+      # else
+      #     echo "- The 'config' folder is missing"
+      # fi
+      # if [ -d "$Client_Pack/mods" ] then
+      #   sleep 1
+      # else
+      #     echo "- The 'mods' folder is missing"
+      # fi
+      # 
+      # if [ -f "$Client_Pack/manifest.json" ]  then
+      #    sleep 1
+      # else
+      #     echo "- The 'manifest.json' file is missing"
+      # fi
+      # */
+      echo
+      read -p "Press ENTER once you have finished copying the files."
+  fi
+done
 # Manifest.json lesen
 
 MANIFEST_VERSION=$(grep -oP '"version"\s*:\s*"\K[^"]+' \
